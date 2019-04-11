a8311eae85529f099952c15182a494dd58bd4922
Andy <AndyShih51@gmail.com>
2019-04-11 05:15:04 -0700

    Reorganize for github

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
+
+echo 'Installing Python packages...'
+pip3 install -r setup/requirements.txt
+
 echo 'Compiling cy_wrangle ...'
 python3 setup.py build_ext --inplace
 cython cy_wrangle.pyx -a
